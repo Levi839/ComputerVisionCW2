71c457cb115d575e7f62a50d7a9b181067154c00
Saman Maghsoudi <21011590@hope.ac.uk>
2024-05-14 22:50:28 +0100

    Remove old codes from Main file

--- cw2.py
+++ cw2.py
@@ -30,6 +30,7 @@ class Stitcher:
         # Step 5 - Warp images to create the panoramic image
         result_with_black_borders = self.warping(img_left, img_right, homography, panorama_size)
 
+        ### Saman ###
         # Display the image with black borders before blending and removing them
         cv2.imshow('Stitched Image with Black Borders', result_with_black_borders)
         cv2.waitKey(0)
@@ -106,59 +107,13 @@ class Stitcher:
     def warping(self, img_left, img_right, homography, output_size=None):
         """
         Warp two images into a single panorama using a given homography matrix.
-
-        This method calculates the necessary transformations to align and stitch two images together into a seamless panorama.
-        It first determines the boundaries of the resulting panorama by transforming the corner points of the right image
-        and combines this with the corner points of the left image.
-
-        Parameters:
-        - img_left: The left source image.
-        - img_right: The right source image.
-        - homography: The homography matrix to transform the right image to align with the left.
-        - output_size: Optional. Specify the size of the output panorama (width, height).
-
-        Returns:
-        - The combined panorama image.
         """
-        h_left, w_left = img_left.shape[:2]
-        h_right, w_right = img_right.shape[:2]
-        corners_left = np.array([
-            [0, 0],
-            [0, h_left - 1],
-            [w_left - 1, h_left - 1],
-            [w_left - 1, 0]
-        ], dtype=np.float32).reshape(-1, 1, 2)
-        corners_right = np.array([
-            [0, 0],
-            [0, h_right - 1],
-            [w_right - 1, h_right - 1],
-            [w_right - 1, 0]
-        ], dtype=np.float32).reshape(-1, 1, 2)
-        warped_corners_right = cv2.perspectiveTransform(corners_right, homography)
-        all_corners = np.concatenate((corners_left, warped_corners_right), axis=0)
-        [x_min, y_min] = np.int32(all_corners.min(axis=0).ravel() - 0.5)
-        [x_max, y_max] = np.int32(all_corners.max(axis=0).ravel() + 0.5)
-        translation_dist = [-x_min, -y_min]
-        H_translation = np.array([[1, 0, translation_dist[0]], [0, 1, translation_dist[1]], [0, 0, 1]])
-        output_img_size = (x_max - x_min, y_max - y_min)
-        warped_image = cv2.warpPerspective(img_right, H_translation.dot(homography), output_img_size)
-        cv2.imshow("Warped Right Image", warped_image)
-        cv2.waitKey(0)
-        for y in range(h_left):
-            for x in range(w_left):
-                if 0 <= x + translation_dist[0] < output_img_size[0] and 0 <= y + translation_dist[1] < output_img_size[
-                    1]:
-                    warped_image[y + translation_dist[1], x + translation_dist[0]] = img_left[y, x]
-        cv2.imshow("Overlayed Left on Warped Right", warped_image)
-        cv2.waitKey(0)
-
-        return warped_image
     
+    ### Saman ###
     def remove_black_border(self, img):
         '''
         Remove black border after stitching
         '''
-        return cropped_image
 
 
 ### Saman ###
@@ -166,43 +121,13 @@ class Blender:
     def linear_blending(self, img1, img2, start_blend, end_blend):
         """
         Linear blending (also known as feathering) across the overlap of two images.
-
-        Parameters:
-        img1 (np.array): The first image in which the blend will be applied.
-        img2 (np.array): The second image from which pixels will be used in the blend.
-        start_blend (int): Start column index for blending in the images.
-        end_blend (int): End column index for blending in the images.
-        
-        Returns:
-        np.array: The first image with the blended region modified.
         """
-        blend_width = end_blend - start_blend
-        for col in range(start_blend, end_blend):
-                alpha = (col - start_blend) / blend_width # Calculate the blend factor for the current column
-                # Update img1's column with a weighted sum of img1's and img2's columns
-                img1[:, col] = cv2.addWeighted(img1[:, col], 1 - alpha, img2[:, col], alpha, 0)
-
-        return img1 # Return the modified first image
 
     ### Saman ###
     def customised_blending(self, img1, img2, start_blend, end_blend):
         """
         Perform custom blending using a sigmoidal curve for smooth transition between two images.
-
-        Parameters:
-        img1 (np.array): The first image where the blending is applied; this image is modified in-place.
-        img2 (np.array): The second image used for blending.
-        start_blend (int): The column index to start blending.
-        end_blend (int): The column index to end blending.
-
-        Returns:
-        np.array: The modified first image with blended region from the second image.
         """
-        blend_width = end_blend - start_blend
-        for col in range(start_blend, end_blend):
-            alpha = 1 / (1 + np.exp(-10 * ((col - start_blend) / blend_width - 0.5)))
-            img1[:, col] = cv2.addWeighted(img1[:, col], 1 - alpha, img2[:, col], alpha, 0)
-        return img1
 
 ## Bob ##
 class Homography:
